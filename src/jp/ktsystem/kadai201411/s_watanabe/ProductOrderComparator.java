b427dc439338a1ec0f279ee8d44c67bae7e602d2
s_watanabe <s_watanabe@K130301.kts-net.ktsystem.jp>
2015-03-31 15:18:43 +0900

    #13 Comparatorのnull対応

--- src/jp/ktsystem/kadai201411/s_watanabe/ProductOrderComparator.java
+++ src/jp/ktsystem/kadai201411/s_watanabe/ProductOrderComparator.java
@@ -2,6 +2,9 @@ package jp.ktsystem.kadai201411.s_watanabe;
 
 import java.util.Comparator;
 
+import jp.ktsystem.kadai201411.common.ErrorCode;
+import jp.ktsystem.kadai201411.common.KadaiException;
+
 /**
  * <p>生産指示ファイルをソートするクラスです。</p>
  *
@@ -18,6 +21,12 @@ public class ProductOrderComparator implements Comparator<ProductOrder> {
         String id1 = o1.getOrderID();
         String id2 = o2.getOrderID();
 
+        if (date1 == null || date2 == null || id1 == null || id2 == null) {
+            try {
+                throw new KadaiException(ErrorCode.INCOMEFILE_FORMAT_ERROR.getErrorCode());
+            } catch (KadaiException e) {
+            }
+        }
         if (date1.equals(date2)) {
             return id1.compareTo(id2);
         } else if (Double.parseDouble(date1) > Double.parseDouble(date2)) {
