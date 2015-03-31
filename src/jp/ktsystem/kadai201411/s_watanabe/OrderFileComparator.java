089c176bc9404ff30de766b74e8abea3fd9313b6
s_watanabe <s_watanabe@K130301.kts-net.ktsystem.jp>
2015-03-31 15:00:20 +0900

    #11 Comparatorのnull対応

--- src/jp/ktsystem/kadai201411/s_watanabe/OrderFileComparator.java
+++ src/jp/ktsystem/kadai201411/s_watanabe/OrderFileComparator.java
@@ -2,6 +2,9 @@ package jp.ktsystem.kadai201411.s_watanabe;
 
 import java.util.Comparator;
 
+import jp.ktsystem.kadai201411.common.ErrorCode;
+import jp.ktsystem.kadai201411.common.KadaiException;
+
 /**
  * <p>受注情報ファイル名をソートするクラスです。</p>
  *
@@ -12,6 +15,15 @@ public class OrderFileComparator implements Comparator<String> {
 
     public int compare(String o1, String o2) {
 
+        if (o1 == null || o2 == null) {
+
+            try {
+                throw new KadaiException(ErrorCode.ORDERFILE_INPUT_ERROR.getErrorCode());
+            } catch (KadaiException e) {
+            }
+
+        }
+
         if (0 == o1.compareToIgnoreCase(o2)) {
 
             return o1.compareTo(o2);
@@ -21,5 +33,6 @@ public class OrderFileComparator implements Comparator<String> {
             return o1.compareToIgnoreCase(o2);
 
         }
+
     }
 }
